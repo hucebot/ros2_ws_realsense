7e0a49f329512d9b73d8ddfb3006e4d3103b2498
leo-berte <leonardo.bertelli96@gmail.com>
2025-10-08 18:01:03 +0200

    updated docker container bash file

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-xhost +local:root
+xhost +local:docker
 
 # I use --volume /dev:/dev, otherwise I have to pass manually each device
 docker run -it --rm \
+    --env DISPLAY=$DISPLAY \
+    --env XAUTHORITY=$XAUTHORITY \
+    --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    --env QT_X11_NO_MITSHM=1 \
+    --env ROS_DOMAIN_ID=39 \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --net host \
+    --ipc host \
+    --pid host \
     --volume /dev:/dev \
     --privileged \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     realsense_ros2
